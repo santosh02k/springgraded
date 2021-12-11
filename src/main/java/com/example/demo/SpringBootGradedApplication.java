2d8830207b948adcd545bb2833f232806376258a
santosh02k <95336552+santosh02k@users.noreply.github.com>
2021-12-11 13:36:46 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/SpringBootGradedApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradedApplication.class, args);
+		System.out.println("graded");
+	}
+
+}
